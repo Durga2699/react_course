7ba62bbaf7bc16923f865ef78a1859b7178305bb
Durag2699 <n190065@rguktn.ac.in>
2025-09-26 22:15:44 +0530

    loops

--- loops.js
+++ loops.js
@@ -1,40 +1,40 @@
-// let fruits = ["Apple", "Banana", "Mango", "Orange"]
-// console.log(fruits[0])
+let fruits = ["Apple", "Banana", "Mango", "Orange"]
+console.log(fruits[0])
 
-// let raji = [ 1,2,3 ];
-// console.log(raji);
+let raji = [ 1,2,3 ];
+console.log(raji);
 
-//  for (let value of  fruits){
-//     console.log(value)
-//  }
+ for (let value of  fruits){
+    console.log(value)
+ }
 
-//  for (let index in fruits){
-//     console.log(index)
-//  }
+ for (let index in fruits){
+    console.log(index)
+ }
 
-//  let object ={
-//     name:"durga",
-//     age:22,
-//     city:"vijayaawada"
+ let object ={
+    name:"durga",
+    age:22,
+    city:"vijayaawada"
 
-//  }
+ }
 
-//  for (let key in object ){
-//     console.log(key)
-//  }
+ for (let key in object ){
+    console.log(key)
+ }
 
-//  for (let key in object){
-//     console.log(object[key])
-//  }
+ for (let key in object){
+    console.log(object[key])
+ }
 
-//  let string="durga"
-//   for (let str in string){
-//     console.log(str)
-//   }
-//    let string1 ="durga"
-//      for (let str1 in string1){
-//         console.log(string1[str1])
-//      }
+ let string="durga"
+  for (let str in string){
+    console.log(str)
+  }
+   let string1 ="durga"
+     for (let str1 in string1){
+        console.log(string1[str1])
+     }
 
 const Fill = new Array(3).fill({x:1})
 Fill[0].x = 2
